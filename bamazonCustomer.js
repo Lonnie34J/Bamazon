c5f28c91b52b0580d7cc7e85eea8c898c2fa4ab2
Lonnie Jones <lonnie34j@gmail.com>
2016-10-07 03:02:26 -0400

    update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,6 +2,8 @@
 
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var cart = [];
+var totalCart = 0;
 
 //Sets up connect to mysql database
 var connection = mysql.createConnection({
@@ -22,7 +24,7 @@ connection.connect(function (err){
 });
 
 //this is the function that wraps up all the code I want to run for the bamazon storefront app
-function startShopping(){
+
 	//this uery will pull all data from the database and then loop through the results to log specified data
 	connection.query("select * from products", function (err, res){
 		for (var i = 0; i<res.length; i++){
@@ -32,6 +34,7 @@ function startShopping(){
 		shop();
 
 	})
+
 
 
 //allows the user to select what items they'd like to purchase and how many
@@ -55,29 +58,40 @@ var shop = function(){
 		//promise function that will handle the transaction 
 		}]).then(function(answer){
 			var query = "Select * From products Where ?" //takes the users input from first question and matches the selected id to info from database then los results
-							connection.query(query, {ItemID: answer.ItemID}, function(err, res){
-								for (var i = 0; i<res.length;i++){
-								console.log("Product: " + res[i].ProductName + ' | ' + "Department: " + res[i].DepartmentName + ' | ' + "Price: " +"$"+ res[i].Price);
-								console.log("----------------------------------------------------------------");
-								//conditional statements: 1. determines if the user selected quanity is greater than the stock in the database 
-									if (answer.Quanity>res[i].StockQuanity){
-										console.log("Insufcient Quanity!");
-									} else{
-										var stockTotal = res[i].StockQuanity - answer.Quanity; //2. subtracts user quanity from database quanity
-										//console.log(stockTotal);
-										var totalCost = res[i].Price * answer.Quanity; //3. multiplies the price by how many of item user enters
-										//console.log(totalCost);
-										connection.query("UPDATE products SET ? WHERE ?", [{StockQuanity: stockTotal}, {ItemID: answer.ItemID}], function(err, res){
-											if (err){throw err}
-												console.log("Your Total is: " +"$"+ totalCost);
-											//invokes the restart function once initial transaction is completed
-												restart();
-										})
-									}
-								}
+            connection.query(query, {ItemID: answer.ItemID}, function(err, res){
+				for (var i = 0; i<res.length;i++){
+				console.log("Product: " + res[i].ProductName + ' | ' + "Department: " + res[i].DepartmentName + ' | ' + "Price: " +"$"+ res[i].Price);
+				console.log("----------------------------------------------------------------");
+				//conditional statements: 1. determines if the user selected quanity is greater than the stock in the database 
+				if (answer.Quanity>res[i].StockQuanity){
+				console.log("Insufcient Quanity!");
+				} 
+                else{
+				
+                var stockTotal = res[i].StockQuanity - answer.Quanity; //2. subtracts user quanity from database quanity
+				//console.log(stockTotal);
+				var totalCost = res[i].Price * answer.Quanity; //3. multiplies the price by how many of item user enters
+				//console.log(totalCost);
+										
+				cart.push(totalCost);
+				console.log(cart);
 
-							})
-		})
+				for (var j = 0; j < cart.length; j++){
+				    totalCart+=cart[j];
+				}
+				console.log(totalCart);
+
+				connection.query("UPDATE products SET ? WHERE ?", [{StockQuanity: stockTotal}, {ItemID: answer.ItemID}], function(err, res){
+				    if (err){throw err}
+				    console.log("Your Total is: " +"$"+ totalCost);
+				    //invokes the restart function once initial transaction is completed
+				    restart();
+				    })
+				}
+            }
+
+        })
+    })
 }
 //this function will prompt the user if they'd wish to continue. If not the app will end
 	var restart = function() {
@@ -99,7 +113,3 @@ var shop = function(){
 			}
 		})
 	}
-
-}
-//starts the whole application
-startShopping();
